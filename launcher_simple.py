0b94ded60065e263fcf8a2e28c45be372e70eb62
河合宏紀 <kawai@lpixel.net>
2021-04-21 16:55:15 +0900

    add files

---
+++ launcher_simple.py
@@ -0,0 +1,332 @@
+import os
+from pathlib import Path
+
+from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QSizePolicy, QLabel, QFileDialog,
+                             QTabWidget, QLineEdit, QCheckBox)
+import numpy as np
+from skimage import io
+
+import utils
+from napari_view_simple import launch_viewers
+
+
+# テキストフォーム中心の画面のためQMainWindowを継承する
+class PredictionsLoader(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.master = parent
+        self.opath = ""
+        self.erpath = ""
+        self.mipath = ""
+        self.btn1 = QPushButton('open', self)
+        self.btn1.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn1.clicked.connect(self.show_dialog_o)
+
+        self.btn2 = QPushButton('open', self)
+        self.btn2.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn2.clicked.connect(self.show_dialog_m)
+
+        self.btn3 = QPushButton('open', self)
+        self.btn3.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn3.clicked.connect(self.show_dialog_e)
+
+        self.textbox = QLineEdit(self)
+        self.textbox.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        self.checkBox = QCheckBox("create new dataset?")
+        self.checkBox.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        self.btn4 = QPushButton('launch napari', self)
+        self.btn4.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn4.clicked.connect(self.launch_napari)
+
+        self.btnb = QPushButton('back', self)
+        self.btnb.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btnb.clicked.connect(self.back)
+
+        self.lbl = QLabel('original dir', self)
+        self.lbl2 = QLabel('mitochondria mask dir', self)
+        self.lbl3 = QLabel('er mask dir', self)
+        self.lbl4 = QLabel('model type (do not use word "train")', self)
+
+        self.build()
+
+    def build(self):
+        vbox = QVBoxLayout()
+        vbox.addWidget(combine_blocks(self.btn1, self.lbl))
+        vbox.addWidget(combine_blocks(self.btn2, self.lbl2))
+        vbox.addWidget(combine_blocks(self.btn3, self.lbl3))
+
+        vbox.addWidget(combine_blocks(self.textbox, self.lbl4))
+
+        vbox.addWidget(self.checkBox)
+
+        vbox.addWidget(self.btn4)
+
+        vbox.addWidget(self.btnb)
+
+        self.setLayout(vbox)
+        self.show()
+
+    def show_dialog_o(self):
+        default_path = max(self.opath, self.erpath, self.mipath, os.path.expanduser('~'))
+        f_name = QFileDialog.getExistingDirectory(self, 'Open Directory', default_path)
+        if f_name:
+            self.opath = f_name
+            self.lbl.setText(self.opath)
+
+    def show_dialog_m(self):
+        default_path = max(self.opath, self.erpath, self.mipath, os.path.expanduser('~'))
+        f_name = QFileDialog.getExistingDirectory(self, 'Open Directory', default_path)
+        if f_name:
+            self.mipath = f_name
+            self.lbl2.setText(self.mipath)
+
+    def show_dialog_e(self):
+        default_path = max(self.opath, self.erpath, self.mipath, os.path.expanduser('~'))
+        f_name = QFileDialog.getExistingDirectory(self, 'Open Directory', default_path)
+        if f_name:
+            self.erpath = f_name
+            self.lbl3.setText(self.erpath)
+
+    def launch_napari(self):
+        images = utils.load_images(self.opath)
+        if self.mipath == "":
+            labels = np.zeros_like(images)
+            view1 = launch_viewers(images, labels, os.path.dirname(os.path.dirname(self.opath)), self.textbox.text(),
+                                   self.checkBox.isChecked())
+        else:
+            labels = utils.load_predicted_masks(self.mipath, self.erpath)
+            view1 = launch_viewers(images, labels, os.path.dirname(os.path.dirname(self.mipath)), self.textbox.text(),
+                                   self.checkBox.isChecked())
+        global view_l
+        view_l.close()
+        return view1
+
+    def back(self):
+        self.master.setCurrentIndex(0)
+
+
+class ModificationsLoader(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.master = parent
+        self.opath = ""
+        self.modpath = ""
+        self.btn1 = QPushButton('open', self)
+        self.btn1.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn1.clicked.connect(self.show_dialog_o)
+        self.btn2 = QPushButton('open', self)
+        self.btn2.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn2.clicked.connect(self.show_dialog_mod)
+
+        self.textbox = QLineEdit(self)
+        self.textbox.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        self.checkBox = QCheckBox("create new dataset?")
+        self.checkBox.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        self.btn4 = QPushButton('launch napari', self)
+        self.btn4.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn4.clicked.connect(self.launch_napari)
+        self.btnb = QPushButton('back', self)
+        self.btnb.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btnb.clicked.connect(self.back)
+        self.lbl = QLabel('original dir', self)
+        self.lbl2 = QLabel('mask dir', self)
+        self.lbl4 = QLabel('model type (do not use word "train")', self)
+        self.build()
+
+    def build(self):
+        vbox = QVBoxLayout()
+        vbox.addWidget(combine_blocks(self.btn1, self.lbl))
+        vbox.addWidget(combine_blocks(self.btn2, self.lbl2))
+        vbox.addWidget(combine_blocks(self.textbox, self.lbl4))
+        vbox.addWidget(self.checkBox)
+        vbox.addWidget(self.btn4)
+        vbox.addWidget(self.btnb)
+
+        self.setLayout(vbox)
+        self.show()
+
+    def show_dialog_o(self):
+        default_path = max(self.opath, self.modpath, os.path.expanduser('~'))
+        f_name = QFileDialog.getExistingDirectory(self, 'Open Directory', default_path)
+        if f_name:
+            self.opath = f_name
+            self.lbl.setText(self.opath)
+
+    def show_dialog_mod(self):
+        default_path = max(self.opath, self.modpath, os.path.expanduser('~'))
+        f_name = QFileDialog.getExistingDirectory(self, 'Open Directory', default_path)
+        if f_name:
+            self.modpath = f_name
+            self.lbl2.setText(self.modpath)
+
+    def back(self):
+        self.master.setCurrentIndex(0)
+
+    def launch_napari(self):
+        images = utils.load_images(self.opath)
+        if self.modpath == "":
+            labels = np.zeros_like(images.compute())
+            self.modpath = os.path.join(os.path.dirname(self.opath), self.textbox.text())
+            os.makedirs(self.modpath, exist_ok=True)
+            filenames = [fn.name for fn in sorted(list(Path(self.opath).glob('./*png')))]
+            for i in range(len(labels)):
+                io.imsave(os.path.join(self.modpath, str(i).zfill(4) + '.png'), labels[i])
+        else:
+            labels = utils.load_saved_masks(self.modpath)
+        try:
+            labels_raw = utils.load_raw_masks(self.modpath + '_raw')
+        except:
+            labels_raw = None
+        view1 = launch_viewers(images, labels, labels_raw, self.modpath, self.textbox.text(), self.checkBox.isChecked())
+        global view_l
+        view_l.close()
+        return view1
+
+
+class InitialLoader(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.master = parent
+        self.opath = ""
+        self.labelpath = ""
+        self.btn1 = QPushButton('open', self)
+        self.btn1.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn1.clicked.connect(self.show_dialog_o)
+        self.btn2 = QPushButton('open', self)
+        self.btn2.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn2.clicked.connect(self.show_dialog_label)
+
+        self.textbox = QLineEdit(self)
+        self.textbox.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        self.checkBox = QCheckBox("create new dataset?")
+        self.checkBox.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        self.btn4 = QPushButton('launch napari', self)
+        self.btn4.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn4.clicked.connect(self.launch_napari)
+        self.btnb = QPushButton('back', self)
+        self.btnb.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btnb.clicked.connect(self.back)
+        self.lbl = QLabel('original dir', self)
+        self.lbl2 = QLabel('label dir', self)
+        self.lbl4 = QLabel('model type (do not use word "train")', self)
+        self.build()
+
+    def build(self):
+        vbox = QVBoxLayout()
+        vbox.addWidget(combine_blocks(self.btn1, self.lbl))
+        vbox.addWidget(combine_blocks(self.btn2, self.lbl2))
+        vbox.addWidget(combine_blocks(self.textbox, self.lbl4))
+        vbox.addWidget(self.checkBox)
+        vbox.addWidget(self.btn4)
+        vbox.addWidget(self.btnb)
+
+        self.setLayout(vbox)
+        self.show()
+
+    def show_dialog_o(self):
+        default_path = max(self.opath, self.labelpath, os.path.expanduser('~'))
+        f_name = QFileDialog.getExistingDirectory(self, 'Open Directory', default_path)
+        if f_name:
+            self.opath = f_name
+            self.lbl.setText(self.opath)
+
+    def show_dialog_label(self):
+        default_path = max(self.opath, self.labelpath, os.path.expanduser('~'))
+        f_name = QFileDialog.getExistingDirectory(self, 'Open Directory', default_path)
+        if f_name:
+            self.labelpath = f_name
+            self.lbl2.setText(self.labelpath)
+
+    def back(self):
+        self.master.setCurrentIndex(0)
+
+    def launch_napari(self):
+        images = utils.load_images(self.opath)
+        labels = np.zeros_like(images)
+        utils.save_masks(labels, self.labelpath)
+        view1 = launch_viewers(images, labels, self.labelpath, self.textbox.text(), self.checkBox.isChecked())
+        global view_l
+        view_l.close()
+        return view1
+
+
+class Entrance(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.master = parent
+        self.btn1 = QPushButton('PredictionsLoader', self)
+        self.btn1.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn1.clicked.connect(self.move_to_predictions_loader)
+        self.btn2 = QPushButton('ModificationsLoader', self)
+        self.btn2.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn2.clicked.connect(self.move_to_modifications_loader)
+        self.btn3 = QPushButton('InitialLoader', self)
+        self.btn3.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.btn3.clicked.connect(self.move_to_initial_loader)
+        self.build()
+
+    def build(self):
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.btn1)
+        vbox.addWidget(self.btn2)
+        vbox.addWidget(self.btn3)
+
+        self.setLayout(vbox)
+        self.show()
+
+    def move_to_predictions_loader(self):
+        self.master.setCurrentIndex(1)
+
+    def move_to_modifications_loader(self):
+        self.master.setCurrentIndex(2)
+
+    def move_to_initial_loader(self):
+        self.master.setCurrentIndex(3)
+
+
+class App(QTabWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("napari launcher")
+        # 1個1個のタブがメニューに対応
+        self.tab1 = Entrance(self)
+        self.tab2 = PredictionsLoader(self)
+        self.tab3 = ModificationsLoader(self)
+        self.tab4 = InitialLoader(self)
+
+        # タブページに追加
+        self.addTab(self.tab1, "Entrance")
+        self.addTab(self.tab2, "PredictionsLoader")
+        self.addTab(self.tab3, "ModificationsLoader")
+        self.addTab(self.tab4, "InitialLoader")
+
+        # タブパネルのボーダーを削除
+        self.setStyleSheet("QTabWidget::pane { border: 0; }")
+        # タブバーを非表示に(↓をコメントすると動きがわかりやすくなるかも)
+        self.tabBar().hide()
+        self.resize(500, 400)
+
+
+def combine_blocks(block1, block2):
+    temp_widget = QWidget()
+    temp_layout = QHBoxLayout()
+    temp_layout.addWidget(block1)
+    temp_layout.addWidget(block2)
+    temp_widget.setLayout(temp_layout)
+    return temp_widget
+
+
+if __name__ == '__main__':
+    import napari
+    with napari.gui_qt():
+        view_l = napari.Viewer()
+        launcher = App()
+        view_l.window.add_dock_widget(launcher, area='right')
+
+
---
+++ napari_view_simple.py
@@ -0,0 +1,166 @@
+import copy
+import os
+from pathlib import Path
+from random import random
+
+import napari
+from PyQt5.QtWidgets import QFileDialog, QSizePolicy, QSlider
+from PyQt5.QtCore import Qt
+from magicgui import magicgui
+import numpy as np
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+from scipy import ndimage
+
+import utils
+from utils import normalize_x, denormalize_y, annotation_to_input
+from model import get_unet_128
+from dock import Datamanager
+
+
+def launch_viewers(original, base, raw, r_path, model_type, checkbox):
+    global slicer
+    global z_pos
+    global view1
+    global layer
+    global images_original
+    global base_label
+    images_original = original
+    base_label = base
+    try:
+        del view1
+        del layer
+    except NameError:
+        pass
+    view1 = napari.view_image(images_original, contrast_limits=[0, 255])
+    view1.add_labels(base_label, name='base')
+    if raw is not None:
+        view1.add_image(ndimage.gaussian_filter(raw, sigma=3), colormap='magenta', name='low_confident',
+                        blending='additive')
+    else:
+        pass
+    labeled = ndimage.label(base_label, structure=np.ones((3, 3, 3)))[0]
+
+    mks, nums = np.unique(labeled, return_counts=True)
+
+    idx_list = list(np.argsort(nums[1:]))
+    nums = np.sort(nums[1:])
+    labeled_sorted = np.zeros_like(labeled)
+    for i, idx in enumerate(idx_list):
+        labeled_sorted = np.where(labeled == mks[1:][idx], i+1, labeled_sorted)
+
+    def label_ct(labeled_array, nums, value):
+        labeled_temp = copy.copy(labeled_array)
+        idx = np.abs(nums - value).argmin()
+        labeled_temp = np.where((labeled_temp < idx) & (labeled_temp != 0), 255, 0)
+        return labeled_temp
+
+    labeled_c = label_ct(labeled_sorted, nums, 10)
+
+    so_layer = view1.add_image(labeled_c, colormap='cyan', name='small_object', blending='additive')
+
+    def calc_object_callback(t_layer, value, labeled_array, nums):
+        t_layer.data = label_ct(labeled_array, nums, value)
+
+    object_slider = QSlider(Qt.Horizontal)
+    object_slider.setMinimum(0)
+    object_slider.setMaximum(100)
+    object_slider.setSingleStep(10)
+    object_slider.setValue(10)
+
+    object_slider.valueChanged[int].connect(lambda value=object_slider: calc_object_callback(so_layer, value,
+                                                                                             labeled_sorted, nums))
+
+    view1.window.add_dock_widget(object_slider, name='object_size_slider', area='left')
+
+    layer = view1.layers[0]
+    layer1 = view1.layers[1]
+
+    @magicgui(dirname={"mode": "d"})
+    def dirpicker(dirname=Path(r_path)):
+        """Take a filename and do something with it."""
+        print("The filename is:", dirname)
+        return dirname
+
+    gui = dirpicker.Gui(show=True)
+    view1.window.add_dock_widget(gui)
+
+    @magicgui(call_button="save")
+    def saver():
+        out_dir = gui.dirname
+        print("The directory is:", out_dir)
+        return utils.save_masks(layer1.data, out_dir)
+
+    gui2 = saver.Gui(show=True)
+    view1.window.add_dock_widget(gui2, area='bottom')
+
+    dmg = Datamanager()
+    dmg.prepare(r_path, model_type, checkbox)
+    view1.window.add_dock_widget(dmg, area='left')
+
+    def update_button(axis_event):
+        axis = axis_event.axis
+        if axis != 0:
+            return
+        slice_num = axis_event.value
+        dmg.update(slice_num)
+
+    view1.dims.events.axis.connect(update_button)
+
+    # draw canvas
+
+    with plt.style.context('dark_background'):
+        canvas = FigureCanvas(Figure(figsize=(3, 15)))
+
+        xy_axes = canvas.figure.add_subplot(3, 1, 1)
+
+        xy_axes.imshow(np.zeros((100, 100), np.uint8))
+        xy_axes.scatter(50, 50, s=10, c='red', alpha=0.15)
+        xy_axes.set_xlabel('x axis')
+        xy_axes.set_ylabel('y axis')
+        yz_axes = canvas.figure.add_subplot(3, 1, 2)
+        yz_axes.imshow(np.zeros((100, 100), np.uint8))
+        yz_axes.scatter(50, 50, s=10, c='red', alpha=0.15)
+        yz_axes.set_xlabel('y axis')
+        yz_axes.set_ylabel('z axis')
+        zx_axes = canvas.figure.add_subplot(3, 1, 3)
+        zx_axes.imshow(np.zeros((100, 100), np.uint8))
+        zx_axes.scatter(50, 50, s=10, c='red', alpha=0.15)
+        zx_axes.set_xlabel('x axis')
+        zx_axes.set_ylabel('z axis')
+
+        # canvas.figure.tight_layout()
+        canvas.figure.subplots_adjust(left=0, bottom=0.1, right=1, top=0.95, wspace=0, hspace=0.4)
+
+    canvas.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Maximum)
+
+    view1.window.add_dock_widget(canvas, area='right')
+
+    @layer.mouse_drag_callbacks.append
+    def update_canvas_canvas(layer, event):
+        if 'shift' in event.modifiers:
+            try:
+                m_point = np.round(layer.coordinates).astype(int)
+                print(m_point)
+                crop_big = crop_img([m_point[0], m_point[1], m_point[2]], layer)
+                xy_axes.imshow(crop_big[50], 'gray')
+                yz_axes.imshow(crop_big.transpose(1, 0, 2)[50], 'gray')
+                zx_axes.imshow(crop_big.transpose(2, 0, 1)[50], 'gray')
+                canvas.draw_idle()
+            except Exception as e:
+                print(e)
+
+    def crop_img(points, layer):
+        min_vals = [x-50 for x in points]
+        max_vals = [x+50 for x in points]
+        yohaku_minus = [n if n < 0 else 0 for n in min_vals]
+        yohaku_plus = [x - layer.data.shape[i] if layer.data.shape[i] < x else 0 for i, x in
+                       enumerate(max_vals)]
+        crop_slice = tuple(slice(np.maximum(0, n), x) for n, x in zip(min_vals, max_vals))
+        crop_temp = layer.data[crop_slice].persist().compute()
+        cropped_img = np.zeros((100, 100, 100), np.uint8)
+        cropped_img[-yohaku_minus[0]:100 - yohaku_plus[0], -yohaku_minus[1]:100 - yohaku_plus[1]
+        , -yohaku_minus[2]:100 - yohaku_plus[2]] = crop_temp
+        return cropped_img
+
