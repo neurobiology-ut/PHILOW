9b1b53422424b9dafece72d8f021635ed86d7a4d
hiroalchem <h.kawai888@gmail.com>
2023-03-01 18:39:30 +0900

    delete tensorflow dependency

--- src/napari_philow/_utils.py
+++ src/napari_philow/_utils.py
@@ -13,9 +13,6 @@ from skimage.filters import gaussian
 import pandas as pd
 from tqdm import tqdm
 
-import tensorflow.keras.backend as K
-from tensorflow.keras.losses import binary_crossentropy
-
 
 def combine_blocks(block1, block2):
     temp_widget = QWidget()
@@ -278,25 +275,6 @@ def select_train_data(dataframe, ori_imgs, label_imgs, ori_filenames):
             train_label_imgs.append(label_img)
     print(ori_filenames)
     return np.array(train_ori_imgs), np.array(train_label_imgs)
-
-
-def dice_coeff(y_true, y_pred):
-    smooth = 1.
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-    return score
-
-
-def dice_loss(y_true, y_pred):
-    loss = 1 - dice_coeff(y_true, y_pred)
-    return loss
-
-
-def bce_dice_loss(y_true, y_pred):
-    loss = binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
-    return loss
 
 
 def divide_imgs(images):
