cc256719c462e0168bcad630b58c3ea093fb4aca
Shogosuga <shogo1219tdj@gmail.com>
2024-01-11 11:27:23 +0900

    mod: changed saving datatype

--- src/napari_philow/_predict.py
+++ src/napari_philow/_predict.py
@@ -91,7 +91,8 @@ def predict_1ax(ori_filenames, net, out_dir, size, device):
         mito_imgs_ave = 255 * pred_large_image(image, net, device, size)
         # threshed
         img = np.where(mito_imgs_ave >= 127, 1, 0)
-        io.imsave(f'{out_dir_merge}/{filename.name}', img.astype('int32'))
+        #io.imsave(f'{out_dir_merge}/{filename.name}', img.astype('int32'))
+        io.imsave(f'{out_dir_merge}/{filename.name}', img.astype('uint8'))
 
         # raw
         img_ = np.where(mito_imgs_ave[:, :] >= 127, mito_imgs_ave, 0)
